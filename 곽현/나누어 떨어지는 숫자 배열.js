9ba423e5e40456f5e1a9a42c3ef73c57a959e93f
kwakhyun <khyun2348@naver.com>
2022-08-05 11:06:13 +0900

    'commit'

--- 곽현/나누어 떨어지는 숫자 배열.js
+++ 곽현/나누어 떨어지는 숫자 배열.js
@@ -28,15 +28,15 @@
 // 3, 2, 6은 10으로 나누어 떨어지지 않습니다. 나누어 떨어지는 원소가 없으므로 [-1]을 리턴합니다.
 
 function solution(arr, divisor) {
-    var answer = [];
-    
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] % divisor == 0) answer.push(arr[i]);
-        }
-    
-    return answer.length == 0 ? [-1] : answer.sort((a, b) => a - b);
+  var answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor == 0) answer.push(arr[i]);
+  }
+
+  return answer.length == 0 ? [-1] : answer.sort((a, b) => a - b);
 }
 
 console.log(solution([5, 9, 7, 10], 5));
 console.log(solution([2, 36, 1, 3], 1));
-console.log(solution([3,2,6], 10));
+console.log(solution([3, 2, 6], 10));
