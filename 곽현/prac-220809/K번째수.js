5e9b85962d7ae90f5ac8bff4ea6c3b37afbfe1ac
kwakhyun <khyun2348@naver.com>
2022-08-10 01:16:17 +0900

    just commit

--- 곽현/prac-220809/K번째수.js
+++ 곽현/prac-220809/K번째수.js
@@ -25,22 +25,11 @@
 // [1, 5, 2, 6, 3, 7, 4]를 4번째부터 4번째까지 자른 후 정렬합니다. [6]의 첫 번째 숫자는 6입니다.
 // [1, 5, 2, 6, 3, 7, 4]를 1번째부터 7번째까지 자릅니다. [1, 2, 3, 4, 5, 6, 7]의 세 번째 숫자는 3입니다.
 
-// 가독성
 function solution(array, commands) {
   return commands.map((v) => {
-    let i = v[0] - 1;
-    let j = v[1];
-    let k = v[2] - 1;
-    return array.slice(i, j).sort((a, b) => a - b)[k];
+    return array.slice(v[0] - 1, v[1]).sort((a, b) => a - b)[v[2] - 1];
   });
 }
-
-// 효율성(속도)
-// function solution(array, commands) {
-//   return commands.map((v) => {
-//     return array.slice(v[0] - 1, v[1]).sort((a, b) => a - b)[v[2] - 1];
-//   });
-// }
 
 console.log(
   solution(
--- 곽현/prac-220809/약수의 개수와 덧셈.js
+++ 곽현/prac-220809/약수의 개수와 덧셈.js
@@ -46,5 +46,21 @@ function solution(left, right) {
   return answer;
 }
 
+// function solution(left, right) {
+//   let answer = 0;
+
+//   for (let i = left; i <= right; i++) {
+//     let count = 0;
+//     for (let j = 1; j <= i ** 0.5; j++) {
+//       if (j * j == i) count++;
+//       else if (i % j == 0) count += 2;
+//     }
+//     if (count % 2 === 0) answer += i;
+//     else answer -= i;
+//   }
+
+//   return answer;
+// }
+
 console.log(solution(13, 17)); // 43
 console.log(solution(24, 27)); // 52
