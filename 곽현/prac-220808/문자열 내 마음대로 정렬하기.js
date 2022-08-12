11c226e6f817daef6ad71e1c234610ac21de32d5
kwakhyun <khyun2348@naver.com>
2022-08-12 21:37:16 +0900

    last commit

--- 곽현/prac-220808/문자열 내 마음대로 정렬하기.js
+++ 곽현/prac-220808/문자열 내 마음대로 정렬하기.js
@@ -24,11 +24,10 @@
 
 function solution(strings, n) {
   return strings.sort((a, b) => {
-    if (a[n] > b[n]) return 1;
-    if (a[n] < b[n]) return -1;
     if (a[n] === b[n]) return a < b ? -1 : 1;
+    return a[n] > b[n] ? 1 : -1;
   });
 }
 
-console.log(solution(["sun", "bed", "car"], 1));
-console.log(solution(["abce", "abcd", "cdx"], 2));
+console.log(solution(["sun", "bed", "car"], 1)); // [ 'car', 'bed', 'sun' ]
+console.log(solution(["abce", "abcd", "cdx"], 2)); // [ 'abcd', 'abce', 'cdx' ]
